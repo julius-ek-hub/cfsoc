711fdb59a9d9de4d047bea4f501883db412820d7
julius-ek-hub <ekanejulius7@gmail.com>
2023-10-07 22:07:54 +0400

    Version changed to 0.2.3

--- update.cmd
+++ update.cmd
@@ -2,5 +2,6 @@
 
 REM This is a comment line.
 
-REM Listing all the files in the directory Program files 
-git add . && git commit -m "Version change" && npm run build
+@REM Listing all the files in the directory Program files 
+set /p Build=<version.txt
+git add . && git commit -m "Version changed to %Build%" && npm run build
