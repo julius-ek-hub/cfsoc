60d3f004a174de92ec7d04e50c1f2f4f37031c7c
julius-ek-hub <ekanejulius7@gmail.com>
2023-10-12 06:50:08 +0400

    Version change from 0.4.0 to 0.4.1

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "view")));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "10mb" }));
 app.use(fileUpload());
 app.use(cors);
 app.use(db);
